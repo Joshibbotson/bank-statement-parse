c5cadd3a72e198c17faa4473ac3f768a454c948b
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-09-06 20:39:32 +0100

    installed express and began factory function class

--- src/statements/StatementFactory.ts
+++ src/statements/StatementFactory.ts
@@ -1,3 +1,26 @@
 // factory class for statements
 
-class StatementFactory {}
+import { Statements } from "./enum/Statements.enum";
+
+class StatementFactory {
+    constructor() {}
+
+    getShoppingResults(
+        formattedCsvData: Record<PropertyKey, string>,
+        statementType: Statements
+    ) {
+        if (!formattedCsvData) {
+            throw new Error("no formatted data");
+        }
+        const shoppingResults = this.factory(formattedCsvData, statementType);
+    }
+
+    private factory(
+        formattedCsvData: Record<PropertyKey, string>,
+        statementType: Statements
+    ) {
+        switch (statementType) {
+            case Statements.NATWEST:
+        }
+    }
+}
